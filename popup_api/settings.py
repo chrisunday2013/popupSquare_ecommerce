dc50fef103ca518b0b1c2335fee0e984735dbea9
Christian Sunday <potentialsunny47@gmail.com>
2022-11-01 01:26:15 +0100

    limitOffSet and PageNumber Pagination

--- popup_api/settings.py
+++ popup_api/settings.py
@@ -135,9 +135,14 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
     
-    # 'DEFAULT_AUTHENTICATION_CLASSES': (
+    # 'DEFAULT_AUTHENTICATION_CLASSES': [
     #     'rest_framework_simplejwt.authentication.JWTAuthentication',
-    # )
+    # ],
+    # 'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.LimitOffsetPagination',
+    # 'PAGE_SIZE':1
+
+    'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE':1
     
 }
 
